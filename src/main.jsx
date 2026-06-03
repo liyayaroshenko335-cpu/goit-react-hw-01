6a320af47e524595d065dbbb07fcf035ed1fe42d
liyayaroshenko335-cpu <liyayaroshenko335@gmail.com>
2026-06-03 14:59:20 +0300

    profile

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './components/App/App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+import React from "react";
+import ReactDOM from "react-dom/client";
+// Імпорт стилів нормалізації
+import "modern-normalize";
+import App from "./components/App/App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
+import App from "./components/App/App"; 
