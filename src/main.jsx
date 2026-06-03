40b661e2814e396e8d0bf4008ea0bec833a63c1e
liyayaroshenko335-cpu <liyayaroshenko335@gmail.com>
2026-06-03 15:33:41 +0300

    fix: cleanup main.jsx and remove duplicates

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './components/App/App.jsx'
-
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-import React from "react";
-import ReactDOM from "react-dom/client";
-// Імпорт стилів нормалізації
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "modern-normalize";
 import App from "./components/App/App.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 );
